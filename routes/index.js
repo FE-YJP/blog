ab8636d82b44c5f606b88c3fed0b1542bb4c3160
xiaobai2233 <550726844@qq.com>
2019-01-27 13:41:25 +0800

    html-1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    username: "xiaobai",
+    isLogin: false
+  });
 });
-
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+router.get("/signin", function (req, res) {
+  res.render("signin");
+});
 module.exports = router;
