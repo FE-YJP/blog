05158a9638f27e12888fd5f9c91e76a7094e0c6d
xiaobai2233 <550726844@qq.com>
2019-01-30 00:33:18 +0800

    cookie

--- public/javascripts/signin.js
+++ public/javascripts/signin.js
@@ -10,12 +10,15 @@ $(function () {
                 alert("用户或密码不正确,请重试");
             }
             if (data == 1) {
+                $.cookie("username", $("#username").val());
                 alert("登陆成功");
                 location.assign("/");
             }
         })
     });
     $("#admin").on("click", function (e) {
+        console.log($("#username").val());
+        $.cookie("test", $("#username").val());
         e.preventDefault();
         $.post("/users/admin", {
             username: $("#username").val(),
